0f21b43bec23048158701892211ff85964ca52d9
Sufyaan <mdsufyaan2006@gmail.com>
2026-07-28 21:08:05 +0530

    Add files via upload
    Day 1 of building SufyPay - Flutter+Firebase Setup, Production level folder structure, Splash screen

---
+++ splash_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() =>
+      _SplashScreenState();
+}
+
+class _SplashScreenState
+    extends State<SplashScreen> {
+
+  @override
+  void initState() {
+
+    super.initState();
+
+    Future.delayed(
+      const Duration(seconds: 2),
+          () {
+
+        // Tomorrow:
+        // Navigate to AuthGate
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment:
+          MainAxisAlignment.center,
+
+          children: [
+
+            const Icon(
+
+              Icons.account_balance_wallet,
+
+              size: 90,
+
+              color: Colors.deepPurple,
+            ),
+
+            const SizedBox(height: 20),
+
+            const Text(
+
+              "SufyPay",
+
+              style: TextStyle(
+
+                fontSize: 34,
+
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            const Text(
+
+              "Fast • Secure • Simple",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
