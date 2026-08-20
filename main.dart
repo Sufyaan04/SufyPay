efa7f785f4bdd7d3a308d319ca726ca7154a5fb7
Sufyaan <mdsufyaan2006@gmail.com>
2026-08-20 22:14:17 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'screens/splash_screen.dart';
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const SufyPay());
+}
+
+class SufyPay extends StatelessWidget {
+
+  const SufyPay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+
+      title: "SufyPay",
+
+      theme: ThemeData(
+
+        primaryColor: Colors.deepPurple,
+
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
+
+        useMaterial3: true,
+      ),
+
+      home: const SplashScreen(),
+    );
+  }
+}
